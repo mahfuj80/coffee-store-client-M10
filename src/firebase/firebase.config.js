dadfd9e36e101c5280c828869c314721a4cd84a0
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-10-14 21:47:35 +0600

    Signup, Signin, firebase, context added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBsj6KlD8whyvTJNwM3VGSxglKHcreYzL4',
+  authDomain: 'coffee-store-50651.firebaseapp.com',
+  projectId: 'coffee-store-50651',
+  storageBucket: 'coffee-store-50651.appspot.com',
+  messagingSenderId: '977169677260',
+  appId: '1:977169677260:web:8e2ea41725e9fe09b2fb69',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,9 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './index.css';
 import AddCoffee from './components/AddCoffee.jsx';
 import UpdateCoffee from './components/UpdateCoffee.jsx';
+import SignUp from './components/SignUp.jsx';
+import SignIn from './components/SignIn.jsx';
+import AuthProvider from './providers/AuthProvider.jsx';
 
 const router = createBrowserRouter([
   {
@@ -21,9 +24,19 @@ const router = createBrowserRouter([
     element: <UpdateCoffee></UpdateCoffee>,
     loader: ({ params }) => fetch(`http://localhost:5000/coffee/${params.id}`),
   },
+  {
+    path: '/signup',
+    element: <SignUp></SignUp>,
+  },
+  {
+    path: '/signin',
+    element: <SignIn></SignIn>,
+  },
 ]);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   </React.StrictMode>
 );
